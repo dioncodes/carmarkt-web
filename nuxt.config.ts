c9c9ae0f3be459743abab1df1aa0978babd60ba8
Dion Purushotham <mail@dion.software>
2026-05-18 13:30:40 -0400

    feat: add visitors.now analytics/tracking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,6 +53,12 @@ export default defineNuxtConfig({
 					rel: 'stylesheet',
 					href: 'https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&family=Manrope:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap'
 				}
+			],
+			script: [
+				{
+					src: 'https://cdn.visitors.now/v.js',
+					'data-token': '8e5d3bed-08f2-452b-8a19-066afb9abe5a'
+				}
 			]
 		}
 	},
